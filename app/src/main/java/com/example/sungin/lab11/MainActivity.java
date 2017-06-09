a0e86afefcd67ec1477362c46ed09ef379a072e8
tjddls1124 <tjddls1124@nate.com>
2017-06-09 21:51:38 +0900

    끝!

--- app/src/main/java/com/example/sungin/lab11/MainActivity.java
+++ app/src/main/java/com/example/sungin/lab11/MainActivity.java
@@ -6,6 +6,8 @@ import android.graphics.BitmapFactory;
 import android.os.Parcelable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
@@ -20,6 +22,8 @@ import java.io.IOException;
 public class MainActivity extends AppCompatActivity {
     CheckBox cb;
     myCanvas myCanvas1;
+    boolean blurred = false;
+    boolean colored = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,58 +35,126 @@ public class MainActivity extends AppCompatActivity {
         cb.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                if(cb.isChecked())myCanvas1.stamp = true;
+                if (cb.isChecked()) myCanvas1.stamp = true;
                 else myCanvas1.stamp = false;
             }
         });
 
 
-
     }
+
     public void init() {
-        cb = (CheckBox)findViewById(R.id.checkBox);
-        myCanvas1 = (myCanvas)findViewById(R.id.CanvasView);
+        cb = (CheckBox) findViewById(R.id.checkBox);
+        myCanvas1 = (myCanvas) findViewById(R.id.CanvasView);
     }
 
     public void onClick(View v) {
-        if( v.getId()==R.id.button_eraser)
+        if (v.getId() == R.id.button_eraser){
+            cb.setChecked(false);
             myCanvas1.clear();
 
-        if(v.getId() == R.id.button_save) {
+        }
+
+        if (v.getId() == R.id.button_save) {
             try {
                 FileOutputStream fileOutputStream = new FileOutputStream(getFilesDir() + "sample.jpg");
-                myCanvas1.mBitmap.compress(Bitmap.CompressFormat.JPEG,100,fileOutputStream);
-                Toast.makeText(this, "저장되었습니다(sample.jpg)",Toast.LENGTH_SHORT).show();
+                myCanvas1.mBitmap.compress(Bitmap.CompressFormat.JPEG, 100, fileOutputStream);
+                Toast.makeText(this, "저장되었습니다(sample.jpg)", Toast.LENGTH_SHORT).show();
 
                 fileOutputStream.close();
 
             } catch (IOException e) {
-                Toast.makeText(this, e.getMessage(),Toast.LENGTH_SHORT).show();
+                Toast.makeText(this, e.getMessage(), Toast.LENGTH_SHORT).show();
                 e.printStackTrace();
             }
 
         }
-        if(v.getId() == R.id.button_open) {
-          String fileName = getFilesDir() + "sample.jpg";
+        if (v.getId() == R.id.button_open) {
+            String fileName = getFilesDir() + "sample.jpg";
             myCanvas1.open(fileName);
         }
-        if(v.getId() == R.id.button_rotate) {
+        if (v.getId() == R.id.button_rotate) {
             cb.setChecked(true);
             myCanvas1.setOperationType("rotate");
         }
-        if(v.getId() == R.id.button_move) {
+        if (v.getId() == R.id.button_move) {
             cb.setChecked(true);
-            myCanvas1.move();
+            myCanvas1.setOperationType("move");
         }
-        if(v.getId() == R.id.button_scale){
+        if (v.getId() == R.id.button_scale) {
             cb.setChecked(true);
-            myCanvas1.scale();
+            myCanvas1.setOperationType("scale");
         }
 
-        if(v.getId() == R.id.button_skew) {
+        if (v.getId() == R.id.button_skew) {
             cb.setChecked(true);
-            myCanvas1.skew();
+            myCanvas1.setOperationType("skew");
         }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        menu.add(0, 0, 0, "Blurring");
+        menu.add(0, 1, 1, "Coloring");
+        menu.add(0, 2, 2, "Pen With Big");
+        menu.add(1, 3, 3, "Pen Color Red");
+        menu.add(2, 4, 4, "Pen Color Blue");
+
+        menu.setGroupCheckable(0, true, false);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == 0) {
+            if (!item.isChecked()) {
+                myCanvas1.setOperationType("bluring");
+                item.setChecked(true);
+                blurred = true;
+            } else {
+                myCanvas1.setOperationType("reset");
+                if (colored)
+                    myCanvas1.setOperationType("coloring");
+
+
+                item.setChecked(false);
+                blurred = false;
+            }
+        }
+        if (item.getItemId() == 1) {
+            if (!item.isChecked()) {
+                myCanvas1.setOperationType("coloring");
+                item.setChecked(true);
+                colored = true;
+            } else {
+                myCanvas1.setOperationType("reset");
+
+                if (blurred)
+                    myCanvas1.setOperationType("bluring");
+
+                item.setChecked(false);
+                colored = false;
+            }
+        }
+        if (item.getItemId() == 4) {
+
+            myCanvas1.setOperationType("blueColor");
+        }
+        if (item.getItemId() == 3) {
+
+            myCanvas1.setOperationType("redColor");
+        }
+        if (item.getItemId() == 2) {
+            if (!item.isChecked()) {
+                myCanvas1.setOperationType("pen_Big");
+                item.setChecked(true);
+            } else {
+                myCanvas1.setOperationType("pen_Small");
+                item.setChecked(false);
+            }
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
 }
--- app/src/main/java/com/example/sungin/lab11/myCanvas.java
+++ app/src/main/java/com/example/sungin/lab11/myCanvas.java
@@ -4,8 +4,11 @@ import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.BlurMaskFilter;
 import android.graphics.Canvas;
 import android.graphics.Color;
+import android.graphics.ColorMatrix;
+import android.graphics.ColorMatrixColorFilter;
 import android.graphics.Paint;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
@@ -24,7 +27,7 @@ public class myCanvas extends View {
     int oldX = -1;
     int oldY = -1;
     boolean stamp = false;
-    public String operationType="";
+    public String operationType = "";
 
     public myCanvas(Context context) {
         super(context);
@@ -68,16 +71,14 @@ public class myCanvas extends View {
             if (event.getAction() == MotionEvent.ACTION_DOWN) {
                 oldY = Y;
                 oldX = X;
-            }
-           else if (event.getAction() == MotionEvent.ACTION_MOVE) {
+            } else if (event.getAction() == MotionEvent.ACTION_MOVE) {
                 if (oldX != -1) {
                     mCanvas.drawLine(oldX, oldY, X, Y, mPaint);
                     invalidate();
                 }
                 oldX = X;
                 oldY = Y;
-            }
-            else if (event.getAction() == MotionEvent.ACTION_UP) {
+            } else if (event.getAction() == MotionEvent.ACTION_UP) {
                 if (oldX != -1) {
                     mCanvas.drawLine(oldX, oldY, X, Y, mPaint);
                     invalidate();
@@ -95,8 +96,33 @@ public class myCanvas extends View {
     protected void onDraw(Canvas canvas) {
         if (mBitmap != null) {
             drawStamp();
-            canvas.drawBitmap(mBitmap, 0, 0, null);
         }
+
+
+        if (operationType.equals("bluring")) {
+            setBluring();
+        }
+        else if (operationType.equals("reset")) {
+            resetPaint();
+        }
+        else if (operationType.equals("blueColor")) {
+            this.mPaint.setColor(Color.BLUE);
+        }
+        else if (operationType.equals("redColor")) {
+            this.mPaint.setColor(Color.RED);
+        }
+        else if (operationType.equals("pen_Big")) {
+            this.mPaint.setStrokeWidth(5);
+
+        }
+        else if (operationType.equals("pen_Small")) {
+            this.mPaint.setStrokeWidth(3);
+        }
+        else if (operationType.equals("coloring")) {
+            setColorFilter();
+        }
+        canvas.drawBitmap(mBitmap, 0, 0, null);
+
     }
 
     public void setOperationType(String operationType) {
@@ -105,50 +131,82 @@ public class myCanvas extends View {
 
     public void clear() {
         mBitmap.eraseColor(Color.WHITE);
+        stamp = false;
         invalidate();
     }
 
     public void open(String file_name) {
         Bitmap bitmap = BitmapFactory.decodeFile(file_name);
-        if(bitmap != null) {
+        if (bitmap != null) {
             clear();
-            int x = bitmap.getWidth()/2 ;
-            int y = bitmap.getHeight()/2;
-            bitmap=  Bitmap.createScaledBitmap(bitmap,x,y,false);
-            int centerX = mCanvas.getWidth()/2 - x/2;
-            int centerY = mCanvas.getHeight()/2 -y/2;
-            mCanvas.drawBitmap(bitmap,centerX,centerY,mPaint);
-        }
-        else Toast.makeText(getContext() , "저장된 파일이 없습니다", Toast.LENGTH_SHORT).show();
+            int x = bitmap.getWidth() / 2;
+            int y = bitmap.getHeight() / 2;
+            bitmap = Bitmap.createScaledBitmap(bitmap, x, y, false);
+            int centerX = mCanvas.getWidth() / 2 - x / 2;
+            int centerY = mCanvas.getHeight() / 2 - y / 2;
+            mCanvas.drawBitmap(bitmap, centerX, centerY, mPaint);
+        } else Toast.makeText(getContext(), "저장된 파일이 없습니다", Toast.LENGTH_SHORT).show();
     }
 
+    public void setBluring() {
+        BlurMaskFilter blur = new BlurMaskFilter(15, BlurMaskFilter.Blur.NORMAL);
+        mPaint.setMaskFilter(blur);
+        invalidate();
+    }
+
+
+    public void resetPaint() {
+        mPaint.reset();
+        invalidate();
+    }
+
+    public void setColorFilter() {
+        float[] matrixarray = {
+                2f, 0f, 0f, 0f, -25f,
+                0f, 2f, 0f, 0f, -25f,
+                0f, 0f, 2f, 0f, -25f,
+                0f, 0f, 0f, 1f, 0f,
+        };
+        ColorMatrix matrix = new ColorMatrix(matrixarray);
+        mPaint.setColorFilter(new
+                ColorMatrixColorFilter(matrix));
+        invalidate();
+    }
 
 
     public void drawStamp() {
         Bitmap bitmap = BitmapFactory.decodeResource(getResources(),
                 R.mipmap.ic_launcher);
 
-        if ( operationType.equals("rotate") )  {
+        if (operationType.equals("rotate")) {
             stamp = true;
-            mCanvas.rotate(30,mCanvas.getWidth()/2 , mCanvas.getHeight()/2);
+            mCanvas.rotate(30, oldX, oldY);
             mCanvas.drawBitmap(bitmap, oldX, oldY, mPaint);
-            mCanvas.rotate(-30,mCanvas.getWidth()/2 , mCanvas.getHeight()/2);
+            mCanvas.rotate(-30, oldX, oldY);
+            oldX = -1; oldY = -1;
+            operationType = "";
+        } else if (operationType.equals("move")) {
+            stamp = true;
+            mCanvas.translate(10, 10);
+            mCanvas.drawBitmap(bitmap, oldX, oldY, mPaint);
+            mCanvas.translate(-10, -10);
+            oldX = -1; oldY = -1;
+            operationType = "";
+        } else if (operationType.equals("scale")) {
+            stamp = true;
+            mCanvas.scale((float) 1.5, (float) 1.5, oldX, oldY);
+            mCanvas.drawBitmap(bitmap, oldX, oldY, mPaint);
+            mCanvas.scale((float) 2 / 3, (float) 2 / 3, oldX, oldY);
+            oldX = -1; oldY = -1;
             operationType = "";
 
-        }
-        else if(stamp == true)
-            mCanvas.drawBitmap(bitmap,oldX,oldY,mPaint);
-    }
-
-    public void move() {
-        stamp = true;
-        mCanvas.translate(10,10);
-    }
-    public void scale() {
-        stamp = true;
-        mCanvas.scale( (float) 1.5, (float) 1.5);
-    }
-    public void skew() {
-        mCanvas.skew(0.2f,0.2f);
+        } else if (operationType.equals("skew")) {
+            mCanvas.skew(0.2f, 0.2f);
+            mCanvas.drawBitmap(bitmap, oldX, oldY, mPaint);
+            mCanvas.skew(-0.2f, -0.2f);
+            oldX = -1; oldY = -1;
+            operationType = "";
+        } else if (stamp == true)
+            mCanvas.drawBitmap(bitmap, oldX, oldY, mPaint);
     }
 }
